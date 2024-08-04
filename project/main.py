e4a7a50ca9043b2c1453424ea611bdcaf79dd23d
SahilPatel010 <sahil9223538388@gmail.com>
2024-08-04 13:38:08 +0000

    upgraded project with errors

--- project/main.py
+++ project/main.py
@@ -18,8 +18,8 @@ import ctypes
 import time
 import requets
 import shutil
-import twilio.rest import client
-import clint.textui import progress
+from twilio.rest import Client
+from clint.textui import progress
 from ecapture import ecapture as ec
 from bs4 import BeautifulSoup4
 from urllib.request import urlopen
@@ -28,5 +28,26 @@ from urllib.request import urlopen
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishme():
+    hour=int(datetime.datetime.now().hour)
+    if (hour>=4 and hour<12):
+        speak("Good Morning")
+    elif (hour>=12 and hour<17):
+        speak("Good Afternoon")
+    elif (hour>=17 and hour<21):
+        speak("Good Evening")
+    else:
+        speak("Good Night")
+
+
+    assisname=("Jarvis")
+    speak("I am your voice Assistant")
+    speak(assisname)
+
 
 
